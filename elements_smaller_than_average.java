cb00cf4238bfd903732957c9bd897948f098146e
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-01 15:41:53 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         int n=x.nextInt();         int arr[]=new int[n];         int i,sum=0,c=0;         for(i=0; i<n; i++)         {             arr[i]=x.nextInt();         }         for(i=0; i<n; i++)         {             sum=sum+arr[i];         }         int avg=sum/n;         for(i=0; i<n; i++)         {             if(arr[i]<=avg)             {                 c++;             }         }         System.out.println(c);     } }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        int n=x.nextInt();
+        int arr[]=new int[n];
+        int i,sum=0,c=0;
+        for(i=0; i<n; i++)
+        {
+            arr[i]=x.nextInt();
+        }
+        for(i=0; i<n; i++)
+        {
+            sum=sum+arr[i];
+        }
+        int avg=sum/n;
+        for(i=0; i<n; i++)
+        {
+            if(arr[i]<=avg)
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
