cae6436be4123f6fbc1b5f8fad560323b1bf1859
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-01 15:17:40 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         int n=x.nextInt();         if(n%2==0)         {             System.out.println("Even");         }         else         {             System.out.println("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        int n=x.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
