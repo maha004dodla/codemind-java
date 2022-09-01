d31462491737d3fcefbeaf767f8e7214dd573c27
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-01 12:40:33 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         int a=x.nextInt();         int b=x.nextInt();         int c=a*b;         System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        int a=x.nextInt();
+        int b=x.nextInt();
+        int c=a*b;
+        System.out.println(c);
+    }
+}
