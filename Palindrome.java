a30fd0583eaa3a2dfd619dd9cccb969bce785743
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-01 16:52:33 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         int m=x.nextInt();         int r,sum=0;         int n=m;         while(n>0)         {             r=n%10;             sum=(sum*10)+r;             n=n/10;         }         if(sum==m)         {             System.out.println("True");         }         else         {             System.out.println("False");         }     } }

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        int m=x.nextInt();
+        int r,sum=0;
+        int n=m;
+        while(n>0)
+        {
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(sum==m)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
