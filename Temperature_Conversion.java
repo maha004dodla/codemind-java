fc2ce15f168ca01c62bf04c84b6ceebfa4c34efb
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-01 13:05:20 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         float c=x.nextInt();         float f=32+(c*9/5);         System.out.format("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        float c=x.nextInt();
+        float f=32+(c*9/5);
+        System.out.format("%.2f",f);
+    }
+}
