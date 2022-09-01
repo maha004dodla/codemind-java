e8a839582c12ae4c8bf10482cc395f77835c8a26
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-01 13:49:50 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         String a=x.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(a);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        String a=x.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(a);
+    }
+}
