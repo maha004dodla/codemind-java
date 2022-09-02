a7ca091d9f2e688348c56cfc45deef627777225b
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-02 11:46:30 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         String s=x.nextLine();         System.out.println(s.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        String s=x.nextLine();
+        System.out.println(s.toLowerCase());
+    }
+}
