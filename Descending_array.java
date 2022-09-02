111b295d6ca2769b359ed2146ca323f0c48a172b
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-02 12:16:34 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         int n=x.nextInt();         int a[]=new int[n];         int i,j,c=0;         for(i=0; i<n; i++)         {             a[i]=x.nextInt();         }         for(i=0; i<(n-1); i++)         {             if(a[i]>a[i+1])                 {                     c++;                 }         }         if(c==(n-1))         {             System.out.println("yes");         }         else         {             System.out.println("no");         }     } }

---
+++ Descending_array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        int n=x.nextInt();
+        int a[]=new int[n];
+        int i,j,c=0;
+        for(i=0; i<n; i++)
+        {
+            a[i]=x.nextInt();
+        }
+        for(i=0; i<(n-1); i++)
+        {
+            if(a[i]>a[i+1])
+                {
+                    c++;
+                }
+        }
+        if(c==(n-1))
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
