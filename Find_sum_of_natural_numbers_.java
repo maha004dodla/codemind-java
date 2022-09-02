8cc4aaf01ace9fddd4782f1486ae87e1f5c50321
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-02 11:52:45 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         int n=x.nextInt();         int a=(n*(n+1))/2;         System.out.println(a);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        int n=x.nextInt();
+        int a=(n*(n+1))/2;
+        System.out.println(a);
+    }
+}
