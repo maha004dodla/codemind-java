75093b8bbf6e85a31dad547a783944066aea1058
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-02 11:43:16 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         long a=x.nextLong();//long is used for bigger values         long b=x.nextLong();         long c=a*b;         System.out.println(c);     } }

---
+++ Multiply_Strings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        long a=x.nextLong();//long is used for bigger values
+        long b=x.nextLong();
+        long c=a*b;
+        System.out.println(c);
+    }
+}
