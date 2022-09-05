1f3312e0ba76e46326176f5016d5ea508fc31b74
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-05 19:58:18 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         int n=x.nextInt();         int a[]=new int[n];         int i,min;         for(i=0; i<n; i++)         {             a[i]=x.nextInt();         }         min=a[0];         for(i=0; i<n; i++)         {             if(a[i]>min)             {                 min=a[i];             }         }         System.out.println(min);
        }
    }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        int n=x.nextInt();
+        int a[]=new int[n];
+        int i,min;
+        for(i=0; i<n; i++)
+        {
+            a[i]=x.nextInt();
+        }
+        min=a[0];
+        for(i=0; i<n; i++)
+        {
+            if(a[i]>min)
+            {
+                min=a[i];
+            }
+        }
+        System.out.println(min);
+        
+    }
+}
