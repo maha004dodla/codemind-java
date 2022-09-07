a81f5b08b6a8dc9ef81e6fc57ca593bacbcddc34
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-07 10:59:41 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         int n=x.nextInt();         int a[]=new int[n];         int i,flag=0;         for(i=0; i<n; i++)         {             a[i]=x.nextInt();         }         for(i=0; i<n; i++)         {             if(a[i]%2!=0)             {                 System.out.print(a[i]+" ");             }         }         for(i=0; i<n; i++)         {             if(a[i]%2==0)             {                 System.out.print(a[i]+" ");             }         }     } }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        int n=x.nextInt();
+        int a[]=new int[n];
+        int i,flag=0;
+        for(i=0; i<n; i++)
+        {
+            a[i]=x.nextInt();
+        }
+        for(i=0; i<n; i++)
+        {
+            if(a[i]%2!=0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        for(i=0; i<n; i++)
+        {
+            if(a[i]%2==0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+    }
+}
