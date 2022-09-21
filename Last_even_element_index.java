f56868a02be797f6c3fd8ec56e3767ffbec3bacc
maha <97425755+maha004dodla@users.noreply.github.com>
2022-09-21 19:54:37 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner x=new Scanner(System.in);         int n=x.nextInt();         int a[]=new int[n];         int i,j,c=0;         for(i=0; i<n; i++)         {             a[i]=x.nextInt();         }         for(i=n-1; i>0; i--)         {             if(a[i]%2==0)             {                 System.out.println(i);                 c++;                 break;             }         }         if(c==0)         {             System.out.println("0");         }     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner x=new Scanner(System.in);
+        int n=x.nextInt();
+        int a[]=new int[n];
+        int i,j,c=0;
+        for(i=0; i<n; i++)
+        {
+            a[i]=x.nextInt();
+        }
+        for(i=n-1; i>0; i--)
+        {
+            if(a[i]%2==0)
+            {
+                System.out.println(i);
+                c++;
+                break;
+            }
+        }
+        if(c==0)
+        {
+            System.out.println("0");
+        }
+    }
+}
