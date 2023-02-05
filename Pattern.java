a2be5c5fdd6c0895647d6820f4d4046a8132e9f7
maha <97425755+maha004dodla@users.noreply.github.com>
2023-02-05 12:00:19 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=0; i<n; i++)         {             for(j=0; j<n; j++)             {                 System.out.format("%d ",i+1);             }             System.out.format(" ");         }     } }

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        n=sc.nextInt();
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                System.out.format("%d ",i+1);
+            }
+            System.out.format("
+");
+        }
+    }
+}
