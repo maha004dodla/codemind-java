ee62e1e26ffa77cbf80e973cc72623f960eb15d6
maha <97425755+maha004dodla@users.noreply.github.com>
2023-02-05 11:51:23 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=0; i<n; i++)         {             for(j=0; j<n; j++)             {                 System.out.print(((char)(i+65))+" ");             }             System.out.format(" ");         }     } }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        n=sc.nextInt();
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                System.out.print(((char)(i+65))+" ");
+            }
+            System.out.format("
+");
+        }
+    }
+}
